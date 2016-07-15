6dcd26387bcdd790797a1c0a3f85d1d9a005995c
Alexander Stolz <astolz97@t-online.de>
2016-07-15 10:13:38 +0200

    Add files via upload
    Second Version of the main file

--- ueb21/Ueb21.java
+++ ueb21/Ueb21.java
@@ -1,70 +1,139 @@
 package ueb21;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.LineNumberReader;
 import java.io.File;
+import java.util.regex.*;
 
 /**
  *
- * @author manuel
+ * @author Manuel Jung; Alexander Stolz
  */
 public class Ueb21 {
+
+    /*Variablen Deklaration*/
+    private static final String MSG_NO_ARGUMENTS = "Keine Argumente übergeben!";
+    private static final String MSG_UNKNOWN_EXCEPTION = "Ein unerwarteter Fehler ist aufgetreten:\n%s\n";
+    private static final String MSG_UNKNOWN_IOEXCEPTION = "Ein unerwarteter IOFehler ist aufgetreten:\n%s\n";
+    private static final String MSG_NOT_FOUND = "Datei konnte nicht gefunden werden!";
+    private static final String MSG_NAME_NOT_FOUND = "Element konnte nicht gefunden werden";
+    private static final String MSG_NO_FILE = "Ist keine einfache Datei!";
+    private static final String MSG_NOT_READ = "Datei ist nicht lesbar";
+    private static final String EQUALS = " = ";
+    private static final String regex = "(.+?)\\s=\\s(.+?)";
+
     private ExpressionTree tree;
     private HashTabelle<String, Integer> table;
 
-    private Ueb21(){
+    /**
+     * Standartkonstruktor
+     */
+    private Ueb21() {
         this.tree = new ExpressionTree();
     }
-    
+
     /**
+     * Main-Methode ruft Start-Methode mit dem auszuführenden Code auf
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        if (args.length <= 0) {
+            System.out.println(MSG_NO_ARGUMENTS);
+            System.exit(1);
+        }
         new Ueb21().start(args);
     }
-    
-    private void start(String[] args){
-        
-    }
-    
-    private File getFile(String name){
-        return null;
-    }
-    
+
     /**
-     * Wertet die Datei aus.
-     * 
-     * @param file 
+     * Start-Methode
+     *
      */
-    private void dateiAuswerten(File file) {
-        
+    private void start(String[] args) {
+        try {
+            File file = getFile(args[0]);
+            analyzeFile(file);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        } catch (Exception e) {
+            System.out.printf(MSG_UNKNOWN_EXCEPTION, e);
+            System.exit(1);
+        }
     }
-    
+
+    /**
+     * Funktion zum Auswerten der übergebenen Datei.
+     *
+     * @param filename
+     * @return file Rückgabewert der Datei
+     * @throws DListIOException
+     */
+    private File getFile(String filename) throws IOException {
+        File file = new File(filename);
+        if (!file.exists()) {
+            throw new IOException(MSG_NOT_FOUND);
+        } else if (!file.isFile()) {
+            throw new IOException(MSG_NO_FILE);
+        } else if (!file.canRead()) {
+            throw new IOException(MSG_NOT_READ);
+        }
+        return file;
+    }
+
+    /**
+     * Funktion zum Auswerten von der Datei. Fängt Exceptions eventueller
+     * Eingabefehler ab.
+     *
+     * @param file Aktuell übergebene Datei
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    private void analyzeFile(File file) throws FileNotFoundException, IOException {
+        int size = 0;
+        FileReader fr = new FileReader(file);
+        LineNumberReader lnr = new LineNumberReader(fr);
+        String line;
+        while ((line = lnr.readLine()) != null) {
+            System.out.println(line);
+            try {
+                line = line.trim();
+                analyzeLine(line);
+                size++;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        this.table = new HashTabelle(size);
+    }
+
     /**
      * Werte Die einzelnen Zeilen aus, wenn Identifier deklariert werden, werden
-     * sie in die Tabelle eingetragen, wenn der ausdruck steht wird dieser mit der Tabelle
-     * an den Expressiontree übergeben, wenn wertzuweisung dann werte in Tabelle eintragen.
-     * Fehlermeldung werfen wenn bei wertzuweisung ein nicht deklarierter identifiert steht.
-     * 
-     * @param zeile 
+     * sie in die Tabelle eingetragen, wenn der ausdruck steht wird dieser mit
+     * der Tabelle an den Expressiontree übergeben, wenn wertzuweisung dann
+     * werte in Tabelle eintragen. Fehlermeldung werfen wenn bei wertzuweisung
+     * ein nicht deklarierter identifiert steht.
+     *
+     * @param zeile
      */
-    private void zeileAuswerten(String zeile) {
-        
+    private void analyzeLine(String zeile) {
+        Pattern r = Pattern.compile(regex);
+        Matcher m = r.matcher(zeile);
+        if (zeile.contains(EQUALS)) {
+            String name = m.group(1);
+            String key = m.group(2);
+            int wert = Integer.valueOf(key);
+            table.insert(name, wert);
+        }
     }
-    
+
     /**
-     * Zählt die Zeilen in denen die Identifier Deklariert werden und erstell
-     * dann eine Hashtabelle mit passender Länge.
-     * 
-     * @param file 
+     * Den Akuellen Baum Ausgeben.
+     *
      */
-    private void tabelleErstellen(File file){
-        
-    }
-    
-    /**
-     * Den Akuellen Baum Ausgeben. 
-     * 
-     */
-    private void display(){
-        
+    private void display() {
+        tree.toString();
     }
 }
