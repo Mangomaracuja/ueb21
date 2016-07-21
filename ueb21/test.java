899486ae546ea93746a061c2715d82847af386da
Niklas Reinhard <niklas.reinhard@googlemail.com>
2016-07-21 10:53:15 +0200

    test

--- ueb21/test.java
+++ ueb21/test.java
@@ -1,27 +1,17 @@
 package ueb21;
 
-import Exceptions.DListException;
-import Exceptions.IdentifierException;
-import Exceptions.NoValueInHashTableException;
+
+import ueb21.TreeNode;
 
 /**
  * Created by niklasreinhard on 18/07/16.
  */
 public class test {
-    public static void main(String[] args) throws DListException, IdentifierException, NoValueInHashTableException {
-        ExpressionTree<String> expTree = new ExpressionTree<>();
-        /*expTree.insertLeft(expTree.getRoot(), new TreeNode("Test2"));
+    public static void main(String[] args) {
+        ExpressionTree<String> expTree = new ExpressionTree<String>(new TreeNode("Test"));
+        expTree.insertLeft(expTree.getRoot(), new TreeNode("Test2"));
         expTree.insertRight(expTree.getRoot(), new TreeNode("Test3"));
-        expTree.printInOrder(expTree.getRoot());*/
-        HashTabelle<String,Integer> ht = new HashTabelle<>(10);
-        
-       
-        
-        ht.insert("hallo", 10);
-        ht.insert("ficken", 20);
-        
-        expTree.generateTree("hallo + ficken", ht);
-      
+
         expTree.printInOrder(expTree.getRoot());
     }
 }
